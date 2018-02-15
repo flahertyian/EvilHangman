25c03a3ad2981d36883757a744f5ae898b615b54
flahertyian <ian206flaherty@gmail.com>
2018-02-15 12:15:45 -0800

    added dictionary one and two, added updateMap method

--- HangmanManager.java
+++ HangmanManager.java
@@ -4,47 +4,65 @@ import java.util.*;
 
 public class HangmanManager{
 
-   private int length;
-   private int max;
-   private List<String> dictionary;
-   private TreeMap<String,Set<String>> word;
-   private String pattern;
-   private SortedSet<Character> guesses;
+   	private int length;
+   	private int max;
+   	private List<String> dictionary;
+   	private TreeMap<String,Set<String>> word;
+   	private String pattern;
+   	private SortedSet<Character> guesses;
    
-   public HangmanManager(List<String> dictionary, int length, int max){
-      this.length = length;
-      this.max = max;
-      updateDictionary();
-      //populateMap();
+   	public HangmanManager(List<String> dictionary, int length, int max){
+      	this.length = length;
+      	this.max = max;
+      	initDictionary();
+    	  //populateMap();
       
-   }
+   	}
    
-   public Set<String> words(){
-      return word.get(pattern);
-   }
+   	public Set<String> words(){
+    	  return word.get(pattern);
+   	}
    
-   public int guessesLeft(){
-      return max;
-   }
+   	public int guessesLeft(){
+    	  return max;
+   	}
    
-   public SortedSet<Character> guesses(){
-      return guesses;
-   }
+   	public SortedSet<Character> guesses(){
+      	return guesses;
+   	}
    
-   public String pattern(){
-   
-   }
+   	public String pattern(){
+   		return pattern;
+   	}
    
    //subtract from max after every wrong guesses
-   public int record(char guess){
-      guesses.add(guess);
-         
+   	public int record(char guess){
+      	guesses.add(guess);
+        updateMap(guess);
+        return 0; //temp return statement to make it compile :)
       
-   }
-   
-   private void updatePattern(){
-      //use another data strucutre to filter everything out first before putting it into a map
-   }
+   	}
+
+
+   	private void updateMap(char g){
+		//I think this might acthualy work!!!!!
+   		for(String s: dictionary){
+   			String tempPattern = "";
+   			for(int x=0; x< s.length(); x++){
+   				if(s.charAt(x) == g){
+   					tempPattern+="g";
+   				}else{
+   					tempPattern+="-";
+   				}
+   			}
+   			if(!word.containsKey(tempPattern)){
+   				word.put(tempPattern, new HashSet<String>());
+   			}
+   			word.get(tempPattern).add(s);
+
+   		}
+
+   	}
    
   
       
@@ -62,11 +80,11 @@ public class HangmanManager{
 //       word.put(key, match);
 //    }
 //   
-   private void updateDictionary(){
-      for(String s: dictionary){
-         if(s.length != length){
-            dictionary.remove(s);
-         }
-      }
-   } 
+   	private void initDictionary(){
+      	for(String s: dictionary){
+         	if(s.length() != length){
+            	dictionary.remove(s);
+         	}
+      	}
+   	} 
 }
